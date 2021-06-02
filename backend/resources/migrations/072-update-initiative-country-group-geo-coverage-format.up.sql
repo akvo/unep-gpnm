564a309e151e4887a7b83dd922f7f39da1832fc4
dedenbangkit <mail@dedenbangkit.com>
2021-06-02 23:09:40 +0700

    [#280] Reformat initiative country group geo-coverage MG-NO.072

---
+++ backend/resources/migrations/072-update-initiative-country-group-geo-coverage-format.up.sql
@@ -0,0 +1,14 @@
+WITH  new_initiative AS (
+    SELECT i.id, json_agg(json_build_object(cg.id::text,cg.name)) AS q24_1
+    FROM (((initiative i
+        JOIN LATERAL jsonb_each_text(i.q24) geo_cov_type(key, value) ON (true)
+        JOIN LATERAL jsonb_array_elements(i.q24_1) regions(value) ON (true)
+        JOIN LATERAL jsonb_each_text(regions.value) regions_text(key, value) ON (true)
+        LEFT JOIN country_group cg ON regions_text.value = cg.name)))
+    WHERE (geo_cov_type.key = 'regional'::text)
+    OR (geo_cov_type.key = 'global with elements in specific areas'::text)
+    GROUP BY i.id, geo_cov_type.value, geo_cov_type.key)
+UPDATE initiative
+SET q24_1 = ni.q24_1
+FROM new_initiative ni
+WHERE initiative.id = ni.id;
