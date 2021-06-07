a48a42e28db1b0ab97de0c429ed9bd5e865e4bcc
dedenbangkit <mail@dedenbangkit.com>
2021-06-08 01:01:46 +0700

    [#280] Clean main seeder comment

--- backend/dev/src/gpml/seeder/main.clj
+++ backend/dev/src/gpml/seeder/main.clj
@@ -662,10 +662,6 @@
                     (assoc topic :data (view-table-of topic))))
                x)))))
 
-  (->> (get-country-group-countries db "country_group_countries")
-       (filter #(= 2 (:country_group %)) ,,,)
-       count)
-
   (require '[clojure.set :as set])
 
   (defn missing-names [names]
@@ -673,30 +669,4 @@
                        (#(map :name %))
                        set)]
       (set/difference (set names) db-names)))
-
-
-  (->> (get-data "country_group_countries")
-       vals
-       (#(map missing-names %))
-       (reduce set/union)
-       (run! println))
-
-  (defn geo-name-mimatches [topic]
-    (let [r-countries (->> (get-data topic)
-                           (#(map :geo_coverage %))
-                           flatten
-                           (filter some?)
-                           (filter #(not(= "" %)))
-                           set
-                           )
-          countries (->> (get-data "countries")
-                         (#(map :name %))
-                         set)]
-      (set/difference r-countries countries)))
-
-  (println "Technologies Geo Coverage")
-  (doseq [item (geo-name-mimatches "technologies")]
-    (println item))
-
-
  )
